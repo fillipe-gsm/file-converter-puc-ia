1d3e209a4f8f4b7174f1817072b85d5dd6e57753
Fillipe Goulart <fillipe.mendes@loggi.com>
2021-11-05 14:48:37 -0300

    Add JSON to CSV conversion support

--- file_converter/json2csv.py
+++ file_converter/json2csv.py
@@ -1,78 +1,79 @@
 """Converts CSV file(s) to a JSON format"""
 import json
 from pathlib import Path
-from typing import Any, Dict, List, Optional, Union
+from typing import Any, Dict, List
 
 
-def csv2json(
+def json2csv(
     input_path: str,
     output_path: str = ".",
     separator: str = ",",
     prefix: str = "",
-) -> List[Dict[str, Any]]:
+) -> List[str]:
     """Converts file(s) from CSV format to JSON
     In case of a directory, all files must have the CSV extension and the same
     separator.
     """
 
-    if Path(input_path).is_dir():
-        file_names = list(input_path.rglob("*.csv"))
+    input_path_pos = Path(input_path)
+    if input_path_pos.is_dir():
+        file_names = list(input_path_pos.rglob("*.json"))
     else:
-        file_names = [input_path]
+        file_names = [input_path_pos]
 
-    json_lists = [
+    csv_lists = [
         _convert_file(file_name, separator=separator)
         for file_name in file_names
     ]
 
-    for file_name, json_list in zip(file_names, json_lists):
-        json_file_name = f"{output_path}/{prefix}{file_name.stem}.json"
-        with open(json_file_name, "w", encoding="utf-8") as f:
-            json.dump(json_list, f, indent=4)
+    for file_name, csv_list in zip(file_names, csv_lists):
+        csv_file_name = f"{output_path}/{prefix}{file_name.stem}.csv"
+        with open(csv_file_name, "w", encoding="utf-8") as f:
+            f.writelines(csv_list)
 
-    return json_lists
+    return csv_lists
 
 
 def _convert_file(
     file_name: str, separator: str = ","
-) -> List[Dict[str, str]]:
+) -> List[str]:
     """Converts file from CSV format to JSON"""
 
-    def _process_line(
-        line: str
-    ) -> Dict[str, Optional[Union[int, float, str]]]:
-        """"""
-        line_values = line.strip().split(separator)
-        d_json = {}
-        for key, value in zip(keys, line_values):
-            d_json[key] = _parse_value(value)
-
-        return d_json
-
     with open(file_name, "r", encoding="utf-8") as f:
-        header = next(f)
-        keys = header.strip().split(separator)
-        json_list = [_process_line(line) for line in f]
+        json_list = json.load(f)
 
-    return json_list
+    csv_list = []
+
+    # Header
+    # We assume all json elements have the same keys, so we can use any of them
+    keys = list(json_list[0].keys())  # convert to list to ensure ordering
+    header_line = separator.join(key for key in keys)
+    header_line += "\n"
+    csv_list.append(header_line)
+
+    # Data lines
+    def write_data_line(json_dict: Dict[str, Any]) -> str:
+        # We explicitly use `json_dict[key]` instead of iterating in the values
+        # to ensure the same order
+        data_line = separator.join(
+            _parse_value(json_dict[key]) for key in keys
+        )
+        data_line += "\n"
+        return data_line
+
+    data_lines = [write_data_line(json_dict) for json_dict in json_list]
+    csv_list.extend(data_lines)
+
+    return csv_list
 
 
-def _parse_value(value: str) -> Optional[Union[int, float, str]]:
-    """Parse an incoming value into a number, string or None
-    Here are the possibilities:
-        - An empty string: set to None to be converted to a null later;
-        - All digits: convert into an integer;
-        - Digits and a decimal point: convert into a float;
-        - Otherwise, return the original string.
+def _parse_value(value: Any) -> str:
+    """Parse a value to string
+    Differently from the CSV -> JSON case, all we need here is to replace
+    `None` with an empty string.
     """
-    if not value:
-        return None
 
-    if value.isdigit():
-        return int(value)
+    if value is None:
+        return ""
 
-    # Finally, we are left with a floating point or a regular string
-    try:
-        return float(value)
-    except ValueError:
-        return value
+    return str(value)
--- tests/test_json2csv.py
+++ tests/test_json2csv.py
@@ -3,25 +3,20 @@ from pathlib import Path
 
 import pytest
 
-from file_converter import json2csv
+from file_converter.json2csv import json2csv
 
 
-@pytest.mark.parametrize(
-  "file_name,separator", [
-    ("tests/data/sample_csv_semi_colon.csv", ";"),
-    ("tests/data/sample_csv_tab.csv", "\t"),
-    ("tests/data/sample_csv_colon.csv", ":"),
-    ("tests/data/sample_csv_comma.csv", ","),
-  ]
-)
-def test_json_conversion_with_single_file(file_name, separator, tmp_path):
+@pytest.mark.parametrize("separator", [";", "\t", ":", ",",])
+def test_json_conversion_with_single_file(separator, tmp_path):
     """
-    The generated JSON file must be such that:
-        - Each element has all headers as keys;
-        - There are as many elements as CSV rows.
+    The generated CSV file must be such that:
+        - The header has all required keys;
+        - The nuber of rows is the same as the number of JSON files plus one
+          (to count the header);
+        - The elements are separated with the specific `separator`.
     """
     # Given
-    # file_name = Path.cwd().joinpath("tests/data/sample_csv.csv")
+    input_path = "tests/data/json_data/sample_json.json"
     expected_keys = {
         "apn",
         "zip_code",
@@ -34,84 +29,85 @@ def test_json_conversion_with_single_file(file_name, separator, tmp_path):
     }
 
     # When
-    json_list = csv2json.csv2json(
-        file_name, separator=separator, output_path=tmp_path
-    )[0]
-
-    # Then
-    assert all(
-        set(json_dict.keys()) == expected_keys for json_dict in json_list
-    )  # each json element has all required keys
-    assert len(json_list) == 3
-
-
-def test_csv_conversion_with_single_file_missing_info(tmp_path):
-    """Same as the previous one, but the missing data must have a `None`.
-    In this sample, the second row has a missing `zip_code` and `price`.
-    """
-    # Given
-    file_name = Path.cwd().joinpath("tests/data/sample_csv_missing_info.csv")
-
-    # When
-    json_list = csv2json.csv2json(
-        file_name, separator=",", output_path=tmp_path
-    )[0]
-
-    # Then
-    json_dict_missing_row = json_list[1]
-    assert json_dict_missing_row["zip_code"] is None
-    assert json_dict_missing_row["price"] is None
-
-
-def test_csv_conversion_with_path(tmp_path):
-    """Each CSV file in a path must be properly converted."""
-
-    # Given
-    input_path = Path.cwd().joinpath("tests/data/sample_dir")
-    num_files = 4
-
-    # When
-    json_lists = csv2json.csv2json(
-        input_path, separator=",", output_path=tmp_path
+    csv_lists = json2csv(
+        input_path, separator=separator, output_path=tmp_path
     )
 
     # Then
-    assert len(json_lists) == num_files
+    assert len(csv_lists) == 1  # only one file
+
+    # Notice the next assert tests the separator as well
+    csv_list = csv_lists[0]
+    returned_keys = csv_list[0].strip().split(separator)
+    assert set(returned_keys) == expected_keys
+
+    assert len(csv_list) == 4
+
+
+def test_json_conversion_with_single_file_missing_info(tmp_path):
+    """Same as the previous one, but the missing data must be empty.
+    In this sample, the second row has a missing `zip_code` and `price`.
+    """
+    # Given
+    input_path = "tests/data/json_data/sample_json_missing_info.json"
+    separator = ","
+
+    # When
+    csv_lists = json2csv(
+        input_path, separator=separator, output_path=tmp_path
+    )
+
+    # Then
+    assert len(csv_lists) == 1  # only one file
+
+    csv_list = csv_lists[0]
+    csv_missing_row_data = csv_list[2].strip().split(separator)
+    assert "" in csv_missing_row_data
+
+
+def test_json_conversion_with_path(tmp_path):
+    """Each CSV file in a path must be properly converted."""
+
+    # Given
+    input_path = "tests/data/json_data/sample_dir"
+    num_files = 4
+
+    # When
+    csv_lists = json2csv(input_path, output_path=tmp_path)
+
+    # Then
+    assert len(csv_lists) == num_files
 
 
 def test_output_file_is_saved_on_disk(tmp_path):
     """The output file must be saved on disk.
     Ensure a file is saved with the proper name, which is the same as the
-    input.
+    input but CSV extension
     """
     # Given
-    file_name = Path.cwd().joinpath("tests/data/sample_csv_comma.csv")
+    input_path = "tests/data/json_data/sample_json.json"
 
     # When
-    csv2json.csv2json(
-        file_name, separator=",", output_path=tmp_path
-    )
+    json2csv(input_path, output_path=tmp_path)
 
     # Then
     existing_files = list(tmp_path.iterdir())
 
     assert len(existing_files) == 1
-    assert existing_files[0].name == "sample_csv_comma.json"
+    assert existing_files[0].name == "sample_json.csv"
 
 
 def test_output_file_is_saved_on_disk_with_prefix(tmp_path):
     """Save as before, but add a prefix to the file name"""
     # Given
-    file_name = Path.cwd().joinpath("tests/data/sample_csv_comma.csv")
+    input_path = "tests/data/json_data/sample_json.json"
     prefix = "banana_"
 
     # When
-    csv2json.csv2json(
-        file_name, separator=",", output_path=tmp_path, prefix=prefix
-    )
+    json2csv(input_path, output_path=tmp_path, prefix=prefix)
 
     # Then
     existing_files = list(tmp_path.iterdir())
 
     assert len(existing_files) == 1
-    assert existing_files[0].name == f"{prefix}sample_csv_comma.json"
+    assert existing_files[0].name == f"{prefix}sample_json.csv"
